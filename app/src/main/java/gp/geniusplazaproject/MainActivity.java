fe6a508d9bcbaeb1d772a4dd3f54bd3da01c44d8
Clifton Robinson <cliffrob25@gmail.com>
2019-05-02 14:32:22 -0600

    Added:
    Internet permission
    Libraries
    
    Model for Users
    
    Added layout for single user
    
    Added Adapter to load User in RecyclerView
    Added placeholder image
    
    Wrote Method to parse json with Volley in MainActivity
    Created recyclerview to display each user
    
    Wrote Method to POST

--- app/src/main/java/gp/geniusplazaproject/MainActivity.java
+++ app/src/main/java/gp/geniusplazaproject/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -17,6 +19,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import Adapter.ProfileAdapter;
 import Model.ProfileModel;
@@ -25,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private ProfileAdapter mProfileAdapter;
-    private ArrayList<ProfileModel> mExampleList;
+    private ArrayList<ProfileModel> mProfileList;
     private RequestQueue mRequestQueue;
 
     String url = "https://reqres.in/api/users";
@@ -39,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        mExampleList = new ArrayList<>();
+        mProfileList = new ArrayList<>();
 
         mRequestQueue = Volley.newRequestQueue(this);
 
@@ -64,11 +68,11 @@ public class MainActivity extends AppCompatActivity {
                                 String imageUrl = data.getString("avatar");
 
 
-                                mExampleList.add(new ProfileModel(id, fName, lName, imageUrl));
+                                mProfileList.add(new ProfileModel(id, fName, lName, imageUrl));
 
                             }
 
-                            mProfileAdapter = new ProfileAdapter(MainActivity.this, mExampleList);
+                            mProfileAdapter = new ProfileAdapter(MainActivity.this, mProfileList);
                             mRecyclerView.setAdapter(mProfileAdapter);
 
                         } catch (JSONException e) {
@@ -83,5 +87,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mRequestQueue.add(request);
+    }
+
+    private  void postProfile( final int id, final String fName, final String lLast, final String avatar) {
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("first_name", fName);
+                params.put("last_name", lLast);
+                params.put("id", String.valueOf(id+1));
+                params.put("avatar", avatar);
+
+                return params;
+            }
+        };
+
+        mRequestQueue.add(postRequest);
     }
 }
