ba8714167853e57093a6cff280a11db8732a2d33
Clifton Robinson <cliffrob25@gmail.com>
2019-05-02 14:27:59 -0600

    Added:
    Internet permission
    Libraries
    
    Model for Users
    
    Added layout for single user
    
    Added Adapter to load User in RecyclerView
    Added placeholder image
    
    Wrote Method to parse json with Volley in MainActivity
    Created recyclerview to display each user

--- app/src/main/java/gp/geniusplazaproject/MainActivity.java
+++ app/src/main/java/gp/geniusplazaproject/MainActivity.java
@@ -2,12 +2,86 @@ package gp.geniusplazaproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import Adapter.ProfileAdapter;
+import Model.ProfileModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private ProfileAdapter mProfileAdapter;
+    private ArrayList<ProfileModel> mExampleList;
+    private RequestQueue mRequestQueue;
+
+    String url = "https://reqres.in/api/users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mExampleList = new ArrayList<>();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        parseJSON();
+    }
+
+    private void parseJSON() {
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("data");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject data = jsonArray.getJSONObject(i);
+
+                                int id = data.getInt("id");
+                                String fName = data.getString("first_name");
+                                String lName = data.getString("last_name");
+                                String imageUrl = data.getString("avatar");
+
+
+                                mExampleList.add(new ProfileModel(id, fName, lName, imageUrl));
+
+                            }
+
+                            mProfileAdapter = new ProfileAdapter(MainActivity.this, mExampleList);
+                            mRecyclerView.setAdapter(mProfileAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mRequestQueue.add(request);
     }
 }
